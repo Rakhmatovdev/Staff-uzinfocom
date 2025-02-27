0120d5c9af3471ba97d2649c969e7f2e1542f594
Xidirboy <xidirboyfayzullayev08@gmail.com>
2025-02-27 16:26:28 +0500

    fix

--- src/components/ui/UFile.tsx
+++ src/components/ui/UFile.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
 import React, { useState } from 'react';
 import { UploadOutlined } from '@ant-design/icons';
 import { Button, Upload, message } from 'antd';
